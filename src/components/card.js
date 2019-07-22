54d1075fa0485a0cea7fcb70d78bca6ea8641ee6
Mario Andres <Marioarendon2@gmail.com>
2019-07-22 17:50:04 -0400

    Add header state

--- src/components/card.js
+++ src/components/card.js
@@ -1,9 +1,9 @@
-import React, { Component } from "react"
+import React from "react"
 import "./card.css"
 
 const Card = props => (
   <div className="Card">
-    <img src={props.image} alt="card image" />
+    <img src={props.image} alt="card" />
     <h3>{props.title}</h3>
     <p>{props.text}</p>
   </div>
--- src/components/header.js
+++ src/components/header.js
@@ -3,25 +3,52 @@ import PropTypes from "prop-types"
 import React from "react"
 import "./header.css"
 
-const Header = ({ siteTitle }) => (
-  <header className="Header">
-    <div className="HeaderGroup">
-      <Link to="/">
-        <img
-          src={require("../images/logo-designcode.svg")}
-          alt="react + design logo"
-          width="30"
-        />
-      </Link>
-      <Link to="/courses">Courses</Link>
-      <Link to="/downloads">Downloads</Link>
-      <Link to="/workshops">Workshops</Link>
-      <Link to="/buy">
-        <button>Buy</button>
-      </Link>
-    </div>
-  </header>
-)
+class Header extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      hasScrolled: false,
+    }
+  }
+
+  componentDidMount() {
+    window.addEventListener("scroll", this.handleScroll)
+  }
+
+  handleScroll = event => {
+    const scrollTop = window.pageYOffset
+
+    if (scrollTop > 50) {
+      this.setState({ hasScrolled: true })
+    } else {
+      this.setState({ hasScrolled: false })
+    }
+  }
+  render() {
+    return (
+      <header
+        className={this.state.hasScrolled ? "Header HeaderScrolled" : "Header"}
+      >
+        <div className="HeaderGroup">
+          <Link to="/">
+            <img
+              src={require("../images/logo-designcode.svg")}
+              alt="react + design logo"
+              width="30"
+            />
+          </Link>
+          <Link to="/courses">Courses</Link>
+          <Link to="/downloads">Downloads</Link>
+          <Link to="/workshops">Workshops</Link>
+          <Link to="/buy">
+            <button>Buy</button>
+          </Link>
+        </div>
+      </header>
+    )
+  }
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
